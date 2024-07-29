531b81eed5ab01d325b157e129c90bdad1b35ff3
Bruno <brunogmr07@gmail.com>
2024-07-29 15:36:43 -0300

    construindo a estrutura

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,12 @@
+import Titulo from '../../components/Title'
+
+const Sidebar = () => (
+  <>
+    <aside>
+      <img src="https://github.com/OBrunooo.png" alt="" />
+      <Titulo>Bruno Gomes</Titulo>
+    </aside>
+  </>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,14 @@
+import Titulo from '../../components/Title'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem, cum
+      sunt! Sit placeat officiis quod asperiores dicta porro? Iure quo
+      aspernatur enim dolorem incidunt doloremque minus mollitia tempore laborum
+    </p>
+  </section>
+)
+
+export default Sobre
